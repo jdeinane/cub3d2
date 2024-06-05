a5f282ec080936fa260904e584cb599137b8e4ef
Jennifer UBALDO <jubaldo@made-f0Dr9s19.clusters.42paris.fr>
2024-06-05 19:14:50 +0200

    norminette gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 04:00:46 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/06/04 14:51:47 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/06/05 19:14:43 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,16 @@ static char	*handle_end_of_line(char *buffer, int depth, int *ret)
 	return (line);
 }
 
+static int	handle_post_read(int ret, int depth, char **line, char *buffer)
+{
+	if (ret == -1 || (ret == 0 && depth == 0))
+		return (ret);
+	*line = handle_end_of_line(buffer, depth, &ret);
+	if (ret > 0)
+		return (1);
+	return (ret);
+}
+
 int	get_next_line(int fd, char **line)
 {
 	char	buffer[1024];
@@ -41,21 +51,21 @@ int	get_next_line(int fd, char **line)
 
 	*line = NULL;
 	depth = 0;
-	while ((ret = read(fd, &buffer[depth], 1)) > 0)
+	ret = 1;
+	while (ret > 0)
 	{
-		if (buffer[depth] == '\n')
-			break ;
-		depth++;
-		if (depth >= (int) sizeof(buffer))
+		ret = read(fd, &buffer[depth], 1);
+		if (ret > 0)
 		{
-			buffer[depth] = '\0';
-			return (-1);
+			if (buffer[depth] == '\n')
+				break ;
+			depth++;
+			if (depth >= (int) sizeof(buffer))
+			{
+				buffer[depth] = '\0';
+				return (-1);
+			}
 		}
 	}
-	if (ret == -1 || (ret == 0 && depth == 0))
-		return (ret);
-	*line = handle_end_of_line(buffer, depth, &ret);
-	if (ret > 0)
-		return (1);
-	return (ret);
+	return (handle_post_read(ret, depth, line, buffer));
 }
