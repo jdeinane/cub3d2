f4bca44a6c0e5cc50e950a40a260c35672299909
Jennifer UBALDO <jubaldo@made-f0Dr9s19.clusters.42paris.fr>
2024-06-05 18:59:32 +0200

    initial position fixed

--- init/init_player.c
+++ init/init_player.c
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 16:24:03 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/05/18 17:12:35 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/06/05 18:57:45 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	init_player(t_cub3d *game)
 {
-	game->player.pos_x = 1.5;
-	game->player.pos_y = 1.5;
 	game->player.dir_x = -1.0;
 	game->player.dir_y = 0.0;
 	game->player.plane_x = 0.0;
