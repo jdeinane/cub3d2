901014c5cb20a29586930e625fac4370fc7bc300
Jennifer UBALDO <jubaldo@bess-f2r4s2.clusters.42paris.fr>
2024-06-09 18:21:28 +0200

    map parsing

--- parsing/validate_map_walls.c
+++ parsing/validate_map_walls.c
@@ -6,39 +6,35 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 12:38:07 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/06/05 15:06:05 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/06/09 18:11:07 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-static void check_horizontal_walls(t_cub3d *game)
+static void	check_horizontal_walls(t_cub3d *game)
 {
 	int x;
 
 	for (x = 0; x < game->map_width; x++)
 	{
 		if (game->map[0][x] != '1' || game->map[game->map_height - 1][x] != '1')
-		{
 			error_exit(game, "Error: Map is not surrounded by walls (horizontal check)");
-		}
 	}
 }
 
-static void check_vertical_walls(t_cub3d *game)
+static void	check_vertical_walls(t_cub3d *game)
 {
 	int y;
 
 	for (y = 0; y < game->map_height; y++)
 	{
 		if (game->map[y][0] != '1' || game->map[y][game->map_width - 1] != '1')
-		{
 			error_exit(game, "Error: Map is not surrounded by walls (vertical check)");
-		}
 	}
 }
 
-static void check_map_width(t_cub3d *game)
+static void	check_map_width(t_cub3d *game)
 {
 	int y;
 	int width;
@@ -47,14 +43,12 @@ static void check_map_width(t_cub3d *game)
 	for (y = 1; y < game->map_height; y++)
 	{
 		if ((int)ft_strlen(game->map[y]) != width)
-		{
 			error_exit(game, "Error: Map rows have inconsistent widths");
-		}
 	}
 	game->map_width = width;
 }
 
-static void validate_rectangular_map(t_cub3d *game)
+static void	validate_rectangular_map(t_cub3d *game)
 {
 	check_map_width(game);
 	check_horizontal_walls(game);
@@ -92,6 +86,12 @@ static void validate_non_rectangular_map(t_cub3d *game)
 					error_exit(game, "Error: Map contains invalid spaces");
 				if (y < game->map_height - 1 && x < (int)ft_strlen(game->map[y + 1]) && game->map[y + 1][x] == ' ')
 					error_exit(game, "Error: Map contains invalid spaces");
+
+				// Ensure '0' is enclosed by '1' above and below
+				if (y > 0 && game->map[y - 1][x] != '1')
+					error_exit(game, "Error: '0' cell not enclosed by '1' above");
+				if (y < game->map_height - 1 && game->map[y + 1][x] != '1')
+					error_exit(game, "Error: '0' cell not enclosed by '1' below");
 			}
 		}
 	}
